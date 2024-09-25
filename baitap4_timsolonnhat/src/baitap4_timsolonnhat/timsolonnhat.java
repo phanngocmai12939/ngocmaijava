7de32ed8b0ce12f8b303a6b4b09811433b90c4c5
phanngocmai12939 <ngocmaitcncc@gmail.com>
2024-09-25 19:40:39 +0700

    baitap4_timsolonnhat

---
+++ baitap4_timsolonnhat/src/baitap4_timsolonnhat/timsolonnhat.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package baitap4_timsolonnhat;
+
+/**
+ * 
+ */
+public class timsolonnhat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+				
+				Scanner sc = new Scanner(System.in);
+				int a, b, c;
+				a = 5;
+				b = 5;
+				c = 5;
+
+				if (a >= b && a >= c) {
+					System.out.println(a);
+				}
+				if (b >= a && b >= c) {
+					System.out.println(b);
+				}
+				if (c >= a && c >= b) {
+					System.out.println(c);
+				}
+
+			}
+		}
+	}
+
+}
