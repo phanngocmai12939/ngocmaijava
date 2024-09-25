8349754df7dffec7f5077b1749f5a5a7ce42e02f
phanngocmai12939 <ngocmaitcncc@gmail.com>
2024-09-25 21:22:36 +0700

    sửa lần 1

--- baitap4_timsolonnhat/src/baitap4_timsolonnhat/timsolonnhat.java
+++ baitap4_timsolonnhat/src/baitap4_timsolonnhat/timsolonnhat.java
@@ -1,36 +1,25 @@
-/**
- * 
- */
 package baitap4_timsolonnhat;
 
-/**
- * 
- */
+import java.util.Scanner;
+
 public class timsolonnhat {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 				
-				Scanner sc = new Scanner(System.in);
-				int a, b, c;
-				a = 5;
-				b = 5;
-				c = 5;
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
 
-				if (a >= b && a >= c) {
-					System.out.println(a);
-				}
-				if (b >= a && b >= c) {
-					System.out.println(b);
-				}
-				if (c >= a && c >= b) {
-					System.out.println(c);
-				}
-
-			}
+		if (a >= b && a >= c) {
+			System.out.println(a);
+		} 
+		else if (b >= c) {
+			System.out.println(b);
+		} 
+		else {
+			System.out.println(c);
 		}
-	}
 
+	}
 }
