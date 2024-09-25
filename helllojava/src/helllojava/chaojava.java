4b0445a2a4bd23755dd76ee44c58e6a65562c88d
phanngocmai12939 <ngocmaitcncc@gmail.com>
2024-09-25 16:28:14 +0700

    vừa mới bổ sung

--- helllojava/src/helllojava/chaojava.java
+++ helllojava/src/helllojava/chaojava.java
@@ -3,8 +3,12 @@ package helllojava;
 public class chaojava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a = 5;
+		int b = 7;
+		int c=a+b;
+		System.out.println("HÔM NAY TÔI BẮT ĐẦU HỌC JAVA");
+		System.out.println("xin chào Java");
+		System.out.println("Tổng là: + c");
 	}
 
 }
