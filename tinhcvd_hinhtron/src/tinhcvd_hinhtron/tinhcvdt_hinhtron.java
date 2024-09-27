3a4ed3c6ba805b88051e705a1a7e0b25f7af039c
phanngocmai12939 <ngocmaitcncc@gmail.com>
2024-09-27 17:39:39 +0700

    tinhcvdt_hinhtron

---
+++ tinhcvd_hinhtron/src/tinhcvd_hinhtron/tinhcvdt_hinhtron.java
@@ -0,0 +1,24 @@
+package tinhcvd_hinhtron;
+
+import java.util.Scanner;
+
+	public class tinhcvdt_hinhtron {		
+			
+		public static double tinhChuVi(double r){
+			return r * 2 * 3.14;
+			}
+			
+			public static double tinhDienTich(double r) {
+				return r * r * 3.14;
+			}
+			
+		public static void main(String[] args) {	
+		double r;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Nhập bán kính hình tròn: ");
+		r = scanner.nextDouble();
+		System.out.println("Diện tích hình tròn là: " + tinhDienTich(r));
+		System.out.println("Chu vi hình tròn là: "+ tinhChuVi(r));
+	}
+
+}
